7d905ed4ea869454683ef823566bd1c08d946d73
posativ <info@posativ.org>
2012-10-16 15:45:02 +0200

    initial commit

---
+++ isso/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+#
+# Copyright 2012 posativ <info@posativ.org>. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# The views and conclusions contained in the software and documentation are
+# those of the authors and should not be interpreted as representing official
+# policies, either expressed or implied, of posativ <info@posativ.org>.
+#
+# lightweight Disqus alternative
+
+__version__ = '0.1'
+
+
+def main():
+    print "Hallo Welt!"
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import sys
+import re
+
+from setuptools import setup, find_packages
+
+version = re.search("__version__ = '([^']+)'",
+                    open('isso/__init__.py').read()).group(1)
+
+setup(
+    name='isso',
+    version=version,
+    author='posativ',
+    author_email='info@posativ.org',
+    packages=find_packages(),
+    zip_safe=True,
+    url='https://github.com/posativ/isso/',
+    license='BSD revised',
+    description='lightweight Disqus alternative',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Internet",
+        "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7"
+    ],
+    install_requires=['flask'],
+    entry_points={
+        'console_scripts':
+            ['isso = isso:main'],
+    },
+)
