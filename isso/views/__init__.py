ab5d9071724628b66fee01598ceadca4cde46b72
Martin Zimmermann <info@posativ.org>
2013-11-08 19:00:01 +0100

    move comments.requires to views package

---
+++ isso/views/__init__.py
@@ -0,0 +1,35 @@
+# -*- encoding: utf-8 -*-
+
+from werkzeug.exceptions import BadRequest
+
+
+class requires:
+    """Verify that the request URL contains and can parse the parameter.
+
+    .. code-block:: python
+
+        @requires(int, "id")
+        def view(..., id):
+            assert isinstance(id, int)
+
+    Returns a 400 Bad Request that contains a specific error message.
+    """
+
+    def __init__(self, type, param):
+        self.param = param
+        self.type = type
+
+    def __call__(self, func):
+        def dec(app, env, req, *args, **kwargs):
+
+            if self.param not in req.args:
+                raise BadRequest("missing %s query" % self.param)
+
+            try:
+                kwargs[self.param] = self.type(req.args[self.param])
+            except TypeError:
+                raise BadRequest("invalid type for %s, expected %s" % (self.param, self.type))
+
+            return func(app, env, req, *args, **kwargs)
+
+        return dec
