f0ee0a18b13c788906c973bb3b2a99594f27a07e
Martin Zimmermann <info@posativ.org>
2013-11-13 14:20:58 +0100

    move unittest helpers into a separate file

---
+++ specs/fixtures.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+
+import json
+
+
+class FakeIP(object):
+
+    def __init__(self, app, ip):
+        self.app = app
+        self.ip = ip
+
+    def __call__(self, environ, start_response):
+        environ['REMOTE_ADDR'] = self.ip
+        return self.app(environ, start_response)
+
+
+class Dummy:
+
+    status = 200
+
+    def __enter__(self):
+        return self
+
+    def read(self):
+        return ''
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+curl = lambda method, host, path: Dummy()
+loads = lambda data: json.loads(data.decode('utf-8'))
