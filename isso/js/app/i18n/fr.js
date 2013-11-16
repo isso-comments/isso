5a20a4a4bfa0c171fbd4ecb5f56a157591c04c12
Sploinga <xavier@sploing.be>
2013-11-16 20:32:21 +0100

    French translation

---
+++ isso/js/app/i18n/fr.js
@@ -0,0 +1,28 @@
+define({
+    "postbox-text": "Insérez votre commentaire ici (au moins 3 lettres)",
+    "postbox-author": "Nom (optionel)",
+    "postbox-email": "Courriel (optionel)",
+    "postbox-submit": "Soumettre",
+
+    "num-comments": "Un commentaire\n{{ n }} commentaires",
+    "no-comments": "Aucun commentaire pour l'instant",
+
+    "comment-reply": "Répondre",
+    "comment-edit": "Éditer",
+    "comment-save": "Enregistrer",
+    "comment-delete": "Supprimer",
+    "comment-confirm": "Confirmer",
+    "comment-close": "Fermer",
+    "comment-cancel": "Annuler",
+    "comment-deleted": "Commentaire supprimé.",
+    "comment-queued": "Commentaire en attente de modération.",
+    "comment-anonymous": "Anonyme",
+
+    "date-now": "À l'instant'",
+    "date-minute": "Il y a une minute \n{{ n }} minutes",
+    "date-hour": "Il y a une heure\n{{ n }} heures ",
+    "date-day": "Hier\n{{ n }} jours auparavant",
+    "date-week": "Il y a une semaine\n{{ n }} semaines",
+    "date-month": "Il y a un mois\n{{ n }} mois",
+    "date-year": "Il y a un an\n{{ n }} ans"
+});
