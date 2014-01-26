14a2f82134261c27d21a30d8bc091f1e5a25434d
Martin Zimmermann <info@posativ.org>
2014-01-26 18:35:52 +0100

    s/tls/starttls/

--- isso/core.py
+++ isso/core.py
@@ -117,7 +117,7 @@ class Config:
         "reload = off", "profile = off",
         "[smtp]",
         "username = ", "password = ",
-        "host = localhost", "port = 587", "security = tls",
+        "host = localhost", "port = 587", "security = starttls",
         "to = ", "from = ",
         "[guard]",
         "enabled = true",
