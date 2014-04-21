344f94e5a02d7c7e9091efd1cef6aad56e0e73cd
Martin Zimmermann <info@posativ.org>
2014-04-21 10:26:09 +0200

    rm forward (reference) arrow

--- isso/js/app/text/svg.js
+++ isso/js/app/text/svg.js
@@ -1,6 +1,5 @@
-define(["text!./forward.svg", "text!./arrow-down.svg", "text!./arrow-up.svg"], function (forward, arrdown, arrup) {
+define(["text!./arrow-down.svg", "text!./arrow-up.svg"], function (arrdown, arrup) {
     return {
-        "forward": forward,
         "arrow-down": arrdown,
         "arrow-up": arrup
     };
