8dbf4d53375f5bee6b1521183182438abd6954c1
Srijan Choudhary <srijan4@gmail.com>
2014-04-23 01:15:16 +0530

    Client side changes for new API
    * Handle hierarchal comments data format
    * New comments fetching parameters: limit, parent, after
    * Link to load more comments

--- isso/js/app/text/html.js
+++ isso/js/app/text/html.js
@@ -1,6 +1,7 @@
-define(["text!./postbox.html", "text!./comment.html"], function (postbox, comment) {
+define(["text!./postbox.html", "text!./comment.html", "text!./comment-loader.html"], function (postbox, comment, comment_loader) {
     return {
         postbox: postbox,
-        comment: comment
+        comment: comment,
+        comment_loader: comment_loader
     };
 });
