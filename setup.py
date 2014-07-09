aa5a45bce052bdcf8bc622928c4f752bd50640c0
Martin Zimmermann <info@posativ.org>
2014-07-09 23:23:30 +0200

    Preparing release 0.9.4

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ else:
 
 setup(
     name='isso',
-    version='0.9.4.dev0',
+    version='0.9.4',
     author='Martin Zimmermann',
     author_email='info@posativ.org',
     packages=find_packages(),
