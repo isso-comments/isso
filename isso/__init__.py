89d6ea076b8ec9056faf037ea34c8022b6146da5
Martin Zimmermann <info@posativ.org>
2014-11-25 22:59:05 +0100

    add --empty-id flag to import weird Disqus exports, fixes #135

--- isso/__init__.py
+++ isso/__init__.py
@@ -212,6 +212,8 @@ def main():
                        help="perform a trial run with no changes made")
     imprt.add_argument("-t", "--type", dest="type", default=None,
                        choices=["disqus", "wordpress"], help="export type")
+    imprt.add_argument("--empty-id", dest="empty_id", action="store_true",
+                       help="workaround for weird Disqus XML exports, #135")
 
     serve = subparser.add_parser("run", help="run server")
 
@@ -228,7 +230,7 @@ def main():
             dbpath = conf.get("general", "dbpath")
 
         mydb = db.SQLite3(dbpath, conf)
-        migrate.dispatch(args.type, mydb, args.dump)
+        migrate.dispatch(args.type, mydb, args.dump, args.empty_id)
 
         sys.exit(0)
 
