dcb7f6457c3efc262b52c43e07daa2b52dd6c3ab
Jelmer Vernooij <jelmer@samba.org>
2015-01-17 21:54:01 +0100

    Add Dutch translations.

---
+++ isso/js/app/i18n/nl.js
@@ -0,0 +1,30 @@
+define({
+    "postbox-text": "Type reactie hier (minstens 3 karakters)",
+    "postbox-author": "Naam (optioneel)",
+    "postbox-email": "E-mail (optioneel)",
+    "postbox-website": "Website (optional)",
+    "postbox-submit": "Versturen",
+
+    "num-comments": "Één reactie\n{{ n }} reacties",
+    "no-comments": "Nog geen reacties",
+
+    "comment-reply": "Antwoorden",
+    "comment-edit": "Bewerken",
+    "comment-save": "Opslaan",
+    "comment-delete": "Verwijderen",
+    "comment-confirm": "Bevestigen",
+    "comment-close": "Sluiten",
+    "comment-cancel": "Annuleren",
+    "comment-deleted": "Reactie verwijderd.",
+    "comment-queued": "Reacties in de wachtrij voor goedkeuring.",
+    "comment-anonymous": "Anoniem",
+    "comment-hidden": "{{ n }} verborgen",
+
+    "date-now": "nu",
+    "date-minute": "een minuut geleden\n{{ n }} minuten geleden",
+    "date-hour": "een uur geleden\n{{ n }} uur geleden",
+    "date-day": "gisteren\n{{ n }} dagen geleden",
+    "date-week": "vorige week\n{{ n }} weken geleden",
+    "date-month": "vorige maand\n{{ n }} maanden geleden",
+    "date-year": "vorig jaar\n{{ n }} jaar geleden"
+});
