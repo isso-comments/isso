c38851a6cd6f0d7da19fc48d4c9ac98596f32600
Steffen Prince <steffen@sprin.io>
2015-10-28 19:39:57 -0700

    Update to misaka 2.0
    Fixes #208

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ import sys
 
 from setuptools import setup, find_packages
 
-requires = ['html5lib==0.9999999', 'itsdangerous', 'Jinja2',
-            'misaka>=1.0,<2.0', 'werkzeug>=0.9']
+requires = ['itsdangerous', 'Jinja2', 'misaka>=2.0,<3.0', 'html5lib<0.9999999',
+            'werkzeug>=0.9']
 
 if sys.version_info < (2, 7):
     raise SystemExit("Python 2 versions < 2.7 are not supported.")
