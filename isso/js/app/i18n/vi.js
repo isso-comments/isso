17747368e1db9b4149e94fb923194d6045ae0c4b
Đinh Xuân Sâm <xuansamdinh.n2i@gmail.com>
2016-01-01 10:48:36 +0700

    Created new branch for translation: vietnamese.
    Added Vietnamese translation.

---
+++ isso/js/app/i18n/vi.js
@@ -0,0 +1,30 @@
+define({
+    "postbox-text": "Type Comment Here (at least 3 chars)",
+    "postbox-author": "Name (optional)",
+    "postbox-email": "E-mail (optional)",
+    "postbox-website": "Website (optional)",
+    "postbox-submit": "Submit",
+
+    "num-comments": "One Comment\n{{ n }} Comments",
+    "no-comments": "No Comments Yet",
+
+    "comment-reply": "Reply",
+    "comment-edit": "Edit",
+    "comment-save": "Save",
+    "comment-delete": "Delete",
+    "comment-confirm": "Confirm",
+    "comment-close": "Close",
+    "comment-cancel": "Cancel",
+    "comment-deleted": "Comment deleted.",
+    "comment-queued": "Comment in queue for moderation.",
+    "comment-anonymous": "Anonymous",
+    "comment-hidden": "{{ n }} Hidden",
+
+    "date-now": "right now",
+    "date-minute": "a minute ago\n{{ n }} minutes ago",
+    "date-hour": "an hour ago\n{{ n }} hours ago",
+    "date-day": "Yesterday\n{{ n }} days ago",
+    "date-week": "last week\n{{ n }} weeks ago",
+    "date-month": "last month\n{{ n }} months ago",
+    "date-year": "last year\n{{ n }} years ago"
+});
