75371bdd43cfc7b663292e5d5d76174a70f58527
Timo Kankare <timo.kankare@iki.fi>
2016-07-16 15:19:02 +0300

    Ordered fi after en.

--- isso/js/app/i18n.js
+++ isso/js/app/i18n.js
@@ -1,8 +1,8 @@
 define(["app/config", "app/i18n/cs", "app/i18n/de", "app/i18n/en",
-        "app/i18n/fr", "app/i18n/hr", "app/i18n/ru", "app/i18n/it",
-        "app/i18n/eo", "app/i18n/sv", "app/i18n/nl", "app/i18n/el_GR",
-        "app/i18n/es", "app/i18n/vi", "app/i18n/zh_CN", "app/i18n/fi" ],
-        function(config, cs, de, en, fr, hr, ru, it, eo, sv, nl, el, es, vi, zh, fi) {
+        "app/i18n/fi", "app/i18n/fr", "app/i18n/hr", "app/i18n/ru",
+        "app/i18n/it", "app/i18n/eo", "app/i18n/sv", "app/i18n/nl",
+        "app/i18n/el_GR", "app/i18n/es", "app/i18n/vi", "app/i18n/zh_CN"],
+        function(config, cs, de, en, fi, fr, hr, ru, it, eo, sv, nl, el, es, vi, zh) {
 
     "use strict";
 
