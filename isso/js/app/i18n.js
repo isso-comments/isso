5e7f010241cfa644773debc7d6c8b120e0a071ac
Martin Zimmermann <info@posativ.org>
2016-10-12 09:32:59 +0200

    add persian to i18n

--- isso/js/app/i18n.js
+++ isso/js/app/i18n.js
@@ -1,9 +1,9 @@
 define(["app/config", "app/i18n/bg", "app/i18n/cs", "app/i18n/de",
-        "app/i18n/en", "app/i18n/fi", "app/i18n/fr", "app/i18n/hr",
-        "app/i18n/ru", "app/i18n/it", "app/i18n/eo", "app/i18n/sv",
-        "app/i18n/nl", "app/i18n/el_GR", "app/i18n/es", "app/i18n/vi",
-        "app/i18n/zh_CN"],
-        function(config, bg, cs, de, en, fi, fr, hr, ru, it, eo, sv, nl, el, es, vi, zh) {
+        "app/i18n/en", "app/i18n/fa", "app/i18n/fi", "app/i18n/fr",
+        "app/i18n/hr", "app/i18n/ru", "app/i18n/it", "app/i18n/eo",
+        "app/i18n/sv", "app/i18n/nl", "app/i18n/el_GR", "app/i18n/es",
+        "app/i18n/vi", "app/i18n/zh_CN"],
+        function(config, bg, cs, de, en, fa, fi, fr, hr, ru, it, eo, sv, nl, el, es, vi, zh) {
 
     "use strict";
 
@@ -16,6 +16,7 @@ define(["app/config", "app/i18n/bg", "app/i18n/cs", "app/i18n/de",
         case "en":
         case "es":
         case "eo":
+        case "fa":
         case "fi":
         case "hr":
         case "it":
@@ -60,6 +61,7 @@ define(["app/config", "app/i18n/bg", "app/i18n/cs", "app/i18n/de",
         en: en,
         eo: eo,
         es: es,
+        fa: fa,
         fi: fi,
         fr: fr,
         it: it,
