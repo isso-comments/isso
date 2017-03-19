4f66192b5220a67149af0059f2665c99573f8696
Pelle Nilsson <pellenilsson@fastmail.fm>
2017-03-19 18:11:49 +0100

    Have client read out shared settings from server

--- isso/js/embed.js
+++ isso/js/embed.js
@@ -27,14 +27,19 @@ require(["app/lib/ready", "app/config", "app/i18n", "app/api", "app/isso", "app/
             return console.log("abort, #isso-thread is missing");
         }
 
-        $("#isso-thread").append($.new('h4'));
-        $("#isso-thread").append(new isso.Postbox(null));
-        $("#isso-thread").append('<div id="isso-root"></div>');
 
         api.fetch($("#isso-thread").getAttribute("data-isso-id"),
             config["max-comments-top"],
             config["max-comments-nested"]).then(
             function(rv) {
+                for (var setting in rv.config) {
+                    config[setting] = rv.config[setting]
+                }
+
+                $("#isso-thread").append($.new('h4'));
+                $("#isso-thread").append(new isso.Postbox(null));
+                $("#isso-thread").append('<div id="isso-root"></div>');
+
                 if (rv.total_replies === 0) {
                     $("#isso-thread > h4").textContent = i18n.translate("no-comments");
                     return;
