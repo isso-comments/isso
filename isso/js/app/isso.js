isso-comments/isso

commit d46abcbf3915e90b3df4e268a30df0138555056c

Auth support: Make authorization methods configurable
It is now also possible to require authorization for all comments.

isso/js/app/isso.js
@@ -104,7 +104,18 @@ define(["app/dom", "app/utils", "app/config", "app/api", "app/jade", "app/i18n",
             openid.updatePostbox(el);
             facebook.updatePostbox(el);
             google.updatePostbox(el);
-            if (!isLoggedIn()) {
+            if (isLoggedIn()) {
+                $(".post-action", el).showInline();
+            } else {
+                if (config["allow-unauthorized"]) {
+                    $(".auth-login-optional", el).showInline();
+                    $(".auth-login-mandatory", el).hide();
+                    $(".post-action", el).showInline();
+                } else {
+                    $(".auth-login-optional", el).hide();
+                    $(".auth-login-mandatory", el).showInline();
+                    $(".post-action", el).hide();
+                }
                 $(".auth-not-loggedin", el).showInline();
                 $(".isso-postbox .avatar", el).hide();
             }

isso/js/app/i18n/en.js
@@ -32,6 +32,7 @@ define({
     "logout": "Logout",
 
     "auth-login": "or login:",
+    "auth-must-login": "Login to comment:",
     "auth-openid": "Authenticate using OpenID Connect",
     "auth-facebook": "Authenticate using Facebook",
     "auth-google": "Authenticate using Google+",

isso/js/app/i18n/sv.js
@@ -27,6 +27,7 @@ define({
     "login": "Logga in",
     "logout": "Logga ut",
     "auth-login": "eller logga in:",
+    "auth-must-login": "Logga in för att kommentera:",
     "auth-openid": "Legitimera dig med OpenID Connect",
     "auth-facebook": "Legitimera dig med Facebook",
     "auth-google": "Legitimera dig med Google+",

isso/js/app/social/facebook.js
@@ -1,4 +1,4 @@
-define(["app/dom", "app/api"], function($, api) {
+define(["app/dom", "app/config", "app/api"], function($, config, api) {
 
     "use strict";
 
@@ -30,12 +30,16 @@ define(["app/dom", "app/api"], function($, api) {
     }
 
     var init = function(isso_ref) {
+        if (!config["facebook-enabled"]) {
+            return;
+        }
+
         isso = isso_ref;
 
         // Called when Facebook SDK has loaded
         window.fbAsyncInit = function() {
             FB.init({
-                appId      : "1561583880825335",
+                appId      : config["facebook-app-id"],
                 cookie     : true,  // enable cookies to allow the server to access the session
                 xfbml      : true,  // parse social plugins on this page
                 version    : "v2.5" // use graph api version 2.5
@@ -76,6 +80,9 @@ define(["app/dom", "app/api"], function($, api) {
     }
 
     var initPostbox = function(el) {
+        if (!config["facebook-enabled"]) {
+            return;
+        }
         updatePostbox(el);
         $(".social-logout-link-facebook", el).on("click", function() {
             FB.logout(function(response) {

isso/js/app/social/google.js
@@ -1,4 +1,4 @@
-define(["app/dom", "app/api"], function($, api) {
+define(["app/dom", "app/config", "app/api"], function($, config, api) {
 
     "use strict";
 
@@ -9,6 +9,10 @@ define(["app/dom", "app/api"], function($, api) {
     var gAuth = null;
 
     var init = function(isso_ref) {
+        if (!config["google-enabled"]) {
+            return;
+        }
+
         isso = isso_ref;
 
         // Load Google API
@@ -45,6 +49,9 @@ define(["app/dom", "app/api"], function($, api) {
     }
 
     var initPostbox = function(el) {
+        if (!config["google-enabled"]) {
+            return;
+        }
         updatePostbox(el);
         $(".social-logout-link-google", el).on("click", function() {
             gAuth.signOut().then(function() {

isso/js/app/social/openid.js
@@ -1,4 +1,4 @@
-define(["app/dom", "app/api", "app/jade", "app/i18n"], function($, api, jade, i18n) {
+define(["app/dom", "app/config", "app/api", "app/jade", "app/i18n"], function($, config, api, jade, i18n) {
 
     "use strict";
 
@@ -8,6 +8,9 @@ define(["app/dom", "app/api", "app/jade", "app/i18n"], function($, api, jade, i1
     var authorData = null;
 
     var init = function(isso_ref) {
+        if (!config["openid-enabled"]) {
+            return;
+        }
         isso = isso_ref;
         window.addEventListener("message", function(event) {
             var origin = event.origin || event.originalEvent.origin;
@@ -26,6 +29,9 @@ define(["app/dom", "app/api", "app/jade", "app/i18n"], function($, api, jade, i1
     }
 
     var updatePostbox = function(el) {
+        if (!config["openid-enabled"]) {
+            return;
+        }
         if (loggedIn) {
             $(".auth-not-loggedin", el).hide();
             $(".auth-loggedin-openid", el).showInline();
@@ -43,6 +49,9 @@ define(["app/dom", "app/api", "app/jade", "app/i18n"], function($, api, jade, i1
     }
 
     var initPostbox = function(el) {
+        if (!config["openid-enabled"]) {
+            return;
+        }
         updatePostbox(el);
         $(".social-logout-link-openid", el).on("click", function() {
             api.openidLogout(sessionID);

isso/js/app/text/postbox.jade
@@ -6,16 +6,19 @@ div(class='isso-postbox')
                 = i18n('postbox-text')
         section(class='auth-section')
             div(class='auth-not-loggedin')
-                p(class='input-wrapper')
-                    input(type='text' name='author' placeholder=i18n('postbox-author')
-                          value=author !== null ? '#{author}' : '')
-                p(class='input-wrapper')
-                    input(type='email' name='email' placeholder=i18n('postbox-email')
-                          value=email != null ? '#{email}' : '')
-                p(class='input-wrapper')
-                    input(type='text' name='website' placeholder=i18n('postbox-website')
-                          value=website != null ? '#{website}' : '')
-                p(class='social-login-separator') #{i18n('auth-login')}
+                div(class='auth-login-optional')
+                    p(class='input-wrapper')
+                        input(type='text' name='author' placeholder=i18n('postbox-author')
+                              value=author !== null ? '#{author}' : '')
+                    p(class='input-wrapper')
+                        input(type='email' name='email' placeholder=i18n('postbox-email')
+                              value=email != null ? '#{email}' : '')
+                    p(class='input-wrapper')
+                        input(type='text' name='website' placeholder=i18n('postbox-website')
+                              value=website != null ? '#{website}' : '')
+                    p(class='social-login-separator') #{i18n('auth-login')}
+                div(class='auth-login-mandatory')
+                    p(class='social-login-separator') #{i18n('auth-must-login')}
                 p(class='social-login-wrapper')
                     a(class='social-login-link-openid' href='#')
                         img(alt=i18n('auth-openid'))

isso/js/embed.js
@@ -28,10 +28,6 @@ require(["app/lib/ready", "app/config", "app/i18n", "app/api", "app/isso", "app/
         }
 
 
-        openid.init(isso);
-        facebook.init(isso);
-        google.init(isso);
-
         api.fetch($("#isso-thread").getAttribute("data-isso-id"),
             config["max-comments-top"],
             config["max-comments-nested"]).then(
@@ -43,6 +39,10 @@ require(["app/lib/ready", "app/config", "app/i18n", "app/api", "app/isso", "app/
                 $("#isso-thread").append($.new('h4'));
                 $("#isso-thread").append(new isso.Postbox(null));
                 $("#isso-thread").append('<div id="isso-root"></div>');
+
+                openid.init(isso);
+                facebook.init(isso);
+                google.init(isso);
 
                 if (rv.total_replies === 0) {
                     $("#isso-thread > h4").textContent = i18n.translate("no-comments");

commit 3d354d41cffdf2284b1328e299be7133e00c501e

Fix: Interface glitches introduced in d46abcb

isso/js/app/isso.js
@@ -91,8 +91,27 @@ define(["app/dom", "app/utils", "app/config", "app/api", "app/jade", "app/i18n",
         openid.initPostbox(el);
         facebook.initPostbox(el);
         google.initPostbox(el);
+        updateLogin(el);
 
         return el;
+    };
+
+    var updateLogin = function(el) {
+        if (isLoggedIn()) {
+            $(".post-action", el).showInline();
+        } else {
+            if (config["allow-unauthorized"]) {
+                $(".auth-login-optional", el).showInline();
+                $(".auth-login-mandatory", el).hide();
+                $(".post-action", el).showInline();
+            } else {
+                $(".auth-login-optional", el).hide();
+                $(".auth-login-mandatory", el).showInline();
+                $(".post-action", el).hide();
+            }
+            $(".auth-not-loggedin", el).showInline();
+            $(".isso-postbox .avatar", el).hide();
+        }
     };
 
     var isLoggedIn = function() {
@@ -104,21 +123,7 @@ define(["app/dom", "app/utils", "app/config", "app/api", "app/jade", "app/i18n",
             openid.updatePostbox(el);
             facebook.updatePostbox(el);
             google.updatePostbox(el);
-            if (isLoggedIn()) {
-                $(".post-action", el).showInline();
-            } else {
-                if (config["allow-unauthorized"]) {
-                    $(".auth-login-optional", el).showInline();
-                    $(".auth-login-mandatory", el).hide();
-                    $(".post-action", el).showInline();
-                } else {
-                    $(".auth-login-optional", el).hide();
-                    $(".auth-login-mandatory", el).showInline();
-                    $(".post-action", el).hide();
-                }
-                $(".auth-not-loggedin", el).showInline();
-                $(".isso-postbox .avatar", el).hide();
-            }
+            updateLogin(el);
         });
     }
 
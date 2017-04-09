3d354d41cffdf2284b1328e299be7133e00c501e
Pelle Nilsson <pellenilsson@fastmail.fm>
2017-04-09 15:56:26 +0200

    Fix: Interface glitches introduced in d46abcb

--- isso/js/app/isso.js
+++ isso/js/app/isso.js
@@ -91,8 +91,27 @@ define(["app/dom", "app/utils", "app/config", "app/api", "app/jade", "app/i18n",
         openid.initPostbox(el);
         facebook.initPostbox(el);
         google.initPostbox(el);
+        updateLogin(el);
 
         return el;
+    };
+
+    var updateLogin = function(el) {
+        if (isLoggedIn()) {
+            $(".post-action", el).showInline();
+        } else {
+            if (config["allow-unauthorized"]) {
+                $(".auth-login-optional", el).showInline();
+                $(".auth-login-mandatory", el).hide();
+                $(".post-action", el).showInline();
+            } else {
+                $(".auth-login-optional", el).hide();
+                $(".auth-login-mandatory", el).showInline();
+                $(".post-action", el).hide();
+            }
+            $(".auth-not-loggedin", el).showInline();
+            $(".isso-postbox .avatar", el).hide();
+        }
     };
 
     var isLoggedIn = function() {
@@ -104,21 +123,7 @@ define(["app/dom", "app/utils", "app/config", "app/api", "app/jade", "app/i18n",
             openid.updatePostbox(el);
             facebook.updatePostbox(el);
             google.updatePostbox(el);
-            if (isLoggedIn()) {
-                $(".post-action", el).showInline();
-            } else {
-                if (config["allow-unauthorized"]) {
-                    $(".auth-login-optional", el).showInline();
-                    $(".auth-login-mandatory", el).hide();
-                    $(".post-action", el).showInline();
-                } else {
-                    $(".auth-login-optional", el).hide();
-                    $(".auth-login-mandatory", el).showInline();
-                    $(".post-action", el).hide();
-                }
-                $(".auth-not-loggedin", el).showInline();
-                $(".isso-postbox .avatar", el).hide();
-            }
+            updateLogin(el);
         });
     }
 
