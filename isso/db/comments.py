e180c7e17ad55eba7cca5c7d250c5059de7ea31b
Pelle Nilsson <pellenilsson@fastmail.fm>
2017-05-14 15:19:35 +0200

    Correct field type for auth_method db column

--- isso/db/comments.py
+++ isso/db/comments.py
@@ -30,7 +30,7 @@ class Comments:
             'CREATE TABLE IF NOT EXISTS comments (',
             '    tid REFERENCES threads(id), id INTEGER PRIMARY KEY, parent INTEGER,',
             '    created FLOAT NOT NULL, modified FLOAT, mode INTEGER, remote_addr VARCHAR,',
-            '    text VARCHAR, auth_method INTEGER, auth_id VARCHAR, author VARCHAR,',
+            '    text VARCHAR, auth_method VARCHAR, auth_id VARCHAR, author VARCHAR,',
             '    email VARCHAR, website VARCHAR, pictureURL VARCHAR,',
             '    likes INTEGER DEFAULT 0, dislikes INTEGER DEFAULT 0, voters BLOB NOT NULL);'])
 
