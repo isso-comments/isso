2990abad5d82d879cd0b849830cb371cccf0c375
cclauss <cclauss@bluewin.ch>
2018-01-25 12:50:01 +0100

    Revert compat.py

--- isso/compat.py
+++ isso/compat.py
@@ -9,16 +9,18 @@ except NameError:        # Python 3
     text_type = str
     string_types = (str, )
 
-try:
-    memoryview = memoryview  # Python 2.7 and Python 3
-except NameError:
-    memoryview = buffer      # Python 2.6
+#try:
+#    memoryview = memoryview  # Python 2.7 and Python 3
+#except NameError:
+#    memoryview = buffer      # Python 2.6
 
 if not PY2K:
+    buffer = memoryview  # noqa: F821 undefined name 'memoryview' in Python 2.6
     filter, map, zip = filter, map, zip
     iteritems = lambda dikt: iter(dikt.items())  # noqa: E731
     from functools import reduce
 else:
+    buffer = buffer
     from itertools import ifilter, imap, izip
     filter, map, zip = ifilter, imap, izip
     iteritems = lambda dikt: dikt.iteritems()   # noqa: E731
