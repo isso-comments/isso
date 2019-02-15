f1dcd17e37bccfe3ec38001ac97ac43b0db54d0f
Chris Warrick <kwpolska@gmail.com>
2019-02-15 15:58:21 +0100

    Enable Polish locale and use Polish plural forms

--- isso/js/app/i18n/pl.js
+++ isso/js/app/i18n/pl.js
@@ -7,11 +7,11 @@ define({
     "postbox-edit": "Edytuj",
     "postbox-submit": "Wyślij",
     "postbox-notification": "Otrzymuj powiadomienia o odpowiedziach na e-mail",
-    
-    "num-comments": "Jeden komentarz\n{{ n }} komentarzy",
-    "no-comments": "Jeszcze nie ma komentarzy",
+
+    "num-comments": "Jeden komentarz\n{{ n }} komentarze\n{{ n }} komentarzy",
+    "no-comments": "Nie ma jeszcze komentarzy",
     "atom-feed": "Kanał Atom",
-    
+
     "comment-reply": "Odpowiedz",
     "comment-edit": "Edytuj",
     "comment-save": "Zapisz",
@@ -22,13 +22,13 @@ define({
     "comment-deleted": "Komentarz usunięty.",
     "comment-queued": "Komentarz w kolejce do moderacji.",
     "comment-anonymous": "Anonim",
-    "comment-hidden": "{{ n }} ukrytych",
-    
+    "comment-hidden": "{{ n }} ukryty\n{{ n }} ukryte\n{{ n }} ukrytych",
+
     "date-now": "teraz",
-    "date-minute": "minutę temu\n{{ n }} minut temu",
-    "date-hour": "godzinę temu\n{{ n }} godzin temu",
+    "date-minute": "minutę temu\n{{ n }} minuty temu\n{{ n }} minut temu",
+    "date-hour": "godzinę temu\n{{ n }} godziny temu\n{{ n }} godzin temu",
     "date-day": "wczoraj\n{{ n }} dni temu",
-    "date-week": "w ubiegłym tygodniu\n{{ n }} tygodni temu",
-    "date-month": "w ubiegłym miesiącu\n{{ n }} miesięcy temu",
-    "date-year": "w ubiegłym roku\n{{ n }} lat temu"
+    "date-week": "w ubiegłym tygodniu\n{{ n }} tygodnie temu\n{{ n }} tygodni temu",
+    "date-month": "w ubiegłym miesiącu\n{{ n }} miesiące temu\n{{ n }} miesięcy temu",
+    "date-year": "w ubiegłym roku\n{{ n }} lata temu\n{{ n }} lat temu"
 });
