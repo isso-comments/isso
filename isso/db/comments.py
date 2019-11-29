1a41076c7cc17b87d8182d62669a81eb7b4036ea
Charl P. Botha (X1E) <cpbotha@vxlabs.com>
2019-11-29 07:57:04 +0200

    Add comment explaining email sanity checks

--- isso/db/comments.py
+++ isso/db/comments.py
@@ -86,6 +86,9 @@ class Comments:
         Search for previously activated comments with this author email.
         """
 
+        # if the user has not entered email, email == None in which case we can't check if they have previous comments
+        # in addition, we will only check for a matching email if the passed string is 3 chars or longer.
+        # we can only do len() on a string, so we use that check, which of course also ensures it's not None
         if type(email) is str and len(email) >= 3:
             # search for any activated comments within the last 6 months by email
             # this SQL should be one of the fastest ways of doing this check
