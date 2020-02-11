4f621d3122f4dca3688e2a1d9a7f77e8ab702c4d
angristan <angristan@pm.me>
2020-02-11 11:12:18 +0100

    Pin werkzeug: >=0.9,<1.0.0
    fix #611

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 from setuptools import setup, find_packages
 
 requires = ['itsdangerous', 'Jinja2', 'misaka>=2.0,<3.0', 'html5lib',
-            'werkzeug>=0.9', 'bleach']
+            'werkzeug>=0.9,<1.0.0', 'bleach']
 
 if sys.version_info < (2, 7):
     raise SystemExit("Python 2 versions < 2.7 are not supported.")
