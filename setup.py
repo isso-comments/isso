e1f77e70409fdac7b6f49ec61fe77c482a5d7628
bookgin <dongsheoil@gmail.com>
2021-07-06 10:20:31 +0800

    Add python 3.9 support.

--- setup.py
+++ setup.py
@@ -27,7 +27,8 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8"
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9"
     ],
     install_requires=requires,
     entry_points={
