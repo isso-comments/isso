0f449ad9814c17a85b26a965143fc0b99b2a9595
Julien <dev@ingeoveritas.com>
2022-01-05 01:08:25 +0100

    Add extra dependencies for dev (pre-commit and flake8)

--- setup.py
+++ setup.py
@@ -45,6 +45,12 @@ setup(
         "Programming Language :: Python :: 3.9"
     ],
     install_requires=requires,
+    extras_require={
+        "dev": [
+                    "pre-commit>=2.15,<2.18",
+                    "flake8>4,<4.1",
+        ],
+    }
     entry_points={
         'console_scripts':
             ['isso = isso:main'],
