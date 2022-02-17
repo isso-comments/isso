6890d8686be35750ac294be1273f3a44183bcc34
ix5 <ix5@users.noreply.github.com>
2022-02-17 12:37:28 +0100

    Revert "Build JavaScript files during python package build" (#798)
    This reverts commit bbad14e8e1384d49cd274f573cfe8ea121fef532.
    
    Reason for revert:
    The change broke `sdist` (source distribution) installs
    (which will attempt to run `make` even when *installing*,
    but neither `Makefile` nor `package.json` are inlcuded in
    the tarball (see `MANIFEST.in`), so the command will fail.

--- setup.py
+++ setup.py
@@ -1,26 +1,11 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
-import os
-import setuptools.command.build_py
-import subprocess
-
 from setuptools import setup, find_packages
 
 requires = ['itsdangerous', 'Jinja2', 'misaka>=2.0,<3.0', 'html5lib',
             'werkzeug>=1.0', 'bleach', 'Flask-Caching>=1.9', 'Flask']
 tests_require = ['pytest', 'pytest-cov']
-
-
-class NpmBuildCommand(setuptools.command.build_py.build_py):
-    """Prefix Python build with node-based asset build"""
-
-    def run(self):
-        # https://tox.wiki/en/latest/config.html#injected-environment-variables
-        if 'TOX_ENV_NAME' not in os.environ:
-            subprocess.check_call(['make', 'init', 'js'])
-        setuptools.command.build_py.build_py.run(self)
-
 
 setup(
     name='isso',
@@ -51,8 +36,5 @@ setup(
     entry_points={
         'console_scripts':
             ['isso = isso:main'],
-    },
-    cmdclass={
-        'build_py': NpmBuildCommand,
     },
 )
