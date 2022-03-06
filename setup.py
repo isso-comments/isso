735860bb0431a123eca5621b1a48d1c5d6dde6b0
ix5 <ix5@users.noreply.github.com>
2022-03-06 13:04:21 +0100

    setup.py: Note compatibility with python 3.10

--- setup.py
+++ setup.py
@@ -40,7 +40,8 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9"
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     install_requires=requires,
     tests_require=tests_require,
