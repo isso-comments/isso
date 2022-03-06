c57ee6ee5094f385743a6a1083fcad645876ec78
fliiiix <hi@l33t.name>
2022-03-06 21:02:00 +0100

    run.py: protect make_app for pytest run

--- isso/run.py
+++ isso/run.py
@@ -1,9 +1,18 @@
 # -*- encoding: utf-8 -*-
 
 import os
+import sys
 import pkg_resources
 
 from isso import config, make_app
+
+# Mock make_app because it is run by pytest
+# with the --doctest-modules flag
+# which will fail because make_app will exit
+# without valid configuration
+# https://stackoverflow.com/a/44595269/1279355
+if "pytest" in sys.modules:
+    make_app = lambda config, multiprocessing: True # noqa
 
 application = make_app(
     config.load(
