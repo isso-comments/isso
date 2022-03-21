2ff3ae56863772a0ff3e1e681d23c5bff796d452
ix5 <ix5@users.noreply.github.com>
2022-03-21 14:36:15 +0100

    isso: js: Replace pug templates with plain js
    This removes dependencies on `pug`, `pug-loader` and
    `pug-runtime` and reduces the js dist bundle to ~56Kb.
    
    This also makes it possible to test templating functionality
    using `Jest`.
    
    Note: Javascript destructuring assignment is not available
    in IE, so this commit drops any compatibility Isso might've
    had.
    See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#browser_compatibility

---
+++ isso/js/app/templates/comment-loader.js
@@ -0,0 +1,9 @@
+var html = function (globals) {
+  // De-structure globals dict
+  ({comment, pluralize} = {...globals});
+  return "" +
+"<div class='isso-comment-loader' id='isso-loader-${comment.name}'>"
++ "<a class='load_hidden' href='#'>" + pluralize('comment-hidden', comment.hidden_replies) + "</a>"
++ "</div>"
+};
+module.exports = html;
