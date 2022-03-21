727dbc333dcef71aa1b9d4571021769223290291
ix5 <ix5@users.noreply.github.com>
2022-03-21 22:09:46 +0100

    js: templates: comment-loader: Correct typo
    This is a remnant of string interpolation in pug templates
    that was overlooked.

--- isso/js/app/templates/comment-loader.js
+++ isso/js/app/templates/comment-loader.js
@@ -2,7 +2,7 @@ var html = function (globals) {
   // De-structure globals dict
   ({comment, pluralize} = {...globals});
   return "" +
-"<div class='isso-comment-loader' id='isso-loader-${comment.name}'>"
+"<div class='isso-comment-loader' id='isso-loader-" + comment.name + "'>"
 + "<a class='load_hidden' href='#'>" + pluralize('comment-hidden', comment.hidden_replies) + "</a>"
 + "</div>"
 };
