4f756e18561b5ba50e59e9ba8b8b65ac6ea87d2a
ix5 <ix5@users.noreply.github.com>
2022-04-30 22:41:08 +0200

    docs: theme: Rework sidebar internals, fix search
    Search was showing no context around items, only titles. To
    fix this, a block with `role="main"` had to be introduced
    at the correct location.
    
    While at it, also make the "Edit on GitHub" link a macro and
    fix associated styling.

--- docs/conf.py
+++ docs/conf.py
@@ -125,6 +125,10 @@ html_theme_options = {
     #"titles_only": True,
 }
 
+# If true, the text around the keyword is shown as summary of each search result.
+# Default is True.
+html_show_search_summary = True
+
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ["."]
 
@@ -163,7 +167,13 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+html_sidebars = {
+    'docs/**': ['sidebar-docs.html'],
+    'index': [],
+    'news': [],
+    'community': [],
+    'search': [],
+}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
