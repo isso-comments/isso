d6b0d2ef10a76a43e04433e6b2696c867380c25e
ix5 <ix5@users.noreply.github.com>
2022-05-23 00:31:34 +0200

    js: test/unit: Correct postbox-reply, tmp skip straggler
    Temporarily disable a test causing failure in the next one,
    because `config.js` is currently not able to re-initialize,
    so changed script params have no effect and next test fails.
    
    Also:
    Usage of `toBeTrue` (no parentheses) was plain wrong. It
    should have been `toBeTruthy()` or `toBe(true)` instead.

--- isso/js/tests/unit/postbox-reply-notif.test.js
+++ isso/js/tests/unit/postbox-reply-notif.test.js
@@ -17,31 +17,58 @@ document.body.innerHTML =
     'data-isso="/" ' +
     '></script>';
 
-const isso = require("app/isso");
 const $ = require("app/dom");
-const config = require("app/config");
-const i18n = require("app/i18n");
-const template = require("app/template");
-template.set("conf", config);
-template.set("i18n", i18n.translate);
 
-test('Create Postbox with reply notifications disabled by default', () => {
+beforeEach(() => {
+  // Clean up any leftover postboxes
+  if ($('.isso-postbox')) {
+    $('.isso-postbox').remove();
+  }
+});
+
+// TODO: Skipped due to interfering with next test
+test.skip('Create Postbox with reply notifications disabled by default', () => {
+  const isso = require("app/isso");
+  const config = require("app/config");
+  const i18n = require("app/i18n");
+  const template = require("app/template");
+  template.set("conf", config);
+  template.set("i18n", i18n.translate);
+
+  expect(config['reply-notifications-default-enabled']).toBe(false);
+
   var isso_thread = $('#isso-thread');
-  isso_thread.append('<div id="isso-root"></div>');
   isso_thread.append(new isso.Postbox(null));
 
   expect($('.isso-notification-section input[type="checkbox"]',
-           isso_thread).checked).toBeFalse;
+           isso_thread).checked()).toBe(false);
 });
 
+// TODO: Due to the current nature of config.js immmediately initializing and
+// not being able to re-initialize, this test can only be run in isolation
 test('Create Postbox with reply notifications enabled by default', () => {
   var script_tag = document.getElementsByTagName('script')[0];
   script_tag.setAttribute('data-isso-reply-notifications-default-enabled', 'true');
 
+  expect(script_tag.getAttribute('data-isso-reply-notifications-default-enabled'))
+    .toBe('true');
+
+  const isso = require("app/isso");
+  const config = require("app/config");
+  const i18n = require("app/i18n");
+  const template = require("app/template");
+  template.set("conf", config);
+  template.set("i18n", i18n.translate);
+
+  expect(config['reply-notifications-default-enabled']).toBe(true);
+
+  expect($('.isso-postbox')).toBe(null);
+
   var isso_thread = $('#isso-thread');
-  isso_thread.append('<div id="isso-root"></div>');
   isso_thread.append(new isso.Postbox(null));
 
+  expect($('.isso-postbox')).toBeTruthy();
+
   expect($('.isso-notification-section input[type="checkbox"]',
-           isso_thread).checked).toBeTrue;
+           isso_thread).checked()).toBe(true);
 });
