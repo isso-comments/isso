5aa1ba088a7b156090303f5ab45e76706107d0c9
ix5 <ix5@users.noreply.github.com>
2022-05-24 00:02:46 +0200

    js: i18n: Add missing German strings
    "Atom-feed" could also be
    "Benachrichtigungen abbonnieren", but it's too long

--- isso/js/app/i18n/de.js
+++ isso/js/app/i18n/de.js
@@ -1,14 +1,20 @@
 module.exports = {
     "postbox-text": "Kommentar hier eingeben (mindestens 3 Zeichen)",
     "postbox-author": "Name (optional)",
+    "postbox-author-placeholder": "Max Mustermann",
     "postbox-email": "E-Mail (optional)",
+    "postbox-email-placeholder": "mustermann@beispiel.de",
     "postbox-website": "Website (optional)",
+    "postbox-website-placeholder": "https://beispiel.de",
     "postbox-preview": "Vorschau",
     "postbox-edit": "Bearbeiten",
     "postbox-submit": "Abschicken",
     "postbox-notification": "wenn auf meinen Kommentar geantwortet wird, möchte ich eine E-Mail bekommen",
+
     "num-comments": "1 Kommentar\n{{ n }} Kommentare",
     "no-comments": "Bisher keine Kommentare",
+    "atom-feed": "Atom-feed",
+
     "comment-reply": "Antworten",
     "comment-edit": "Bearbeiten",
     "comment-save": "Speichern",
@@ -20,6 +26,8 @@ module.exports = {
     "comment-queued": "Kommentar muss noch freigeschaltet werden.",
     "comment-anonymous": "Anonym",
     "comment-hidden": "{{ n }} versteckt",
+    "comment-page-author-suffix": "Autor",
+
     "date-now": "eben gerade",
     "date-minute": "vor einer Minute\nvor {{ n }} Minuten",
     "date-hour": "vor einer Stunde\nvor {{ n }} Stunden",
