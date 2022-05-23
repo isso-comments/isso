c867d3f26cf257a5f3586adf4974af81bc331810
ix5 <ix5@users.noreply.github.com>
2022-05-24 00:53:48 +0200

    js: test: Add boilerplate jest-puppeteer conf
    Currently unused, but useful for quick debugging &
    adjustments during E2E testing.

---
+++ isso/js/jest-puppeteer.config.js
@@ -0,0 +1,14 @@
+/* Configuration specific to `jest-puppeteer`, for E2E integration tests
+ * Could be used to set e.g. Firefox as test runner
+ * https://github.com/smooth-code/jest-puppeteer */
+
+/*
+module.exports = {
+  launch: {
+    dumpio: true,
+    headless: process.env.HEADLESS !== 'false',
+    product: 'chrome',
+  },
+  browserContext: 'default',
+}
+*/
