705c319484494c35c540480f616969af630feea5
ix5 <ix5@users.noreply.github.com>
2022-05-27 04:57:20 +0200

    js: test/integration: Add screenshot comparison script
    Relies on imagemagick to strip out metadata and only compare
    the image contents (via known hashes)
    
    Screenshots generated by running `npm run test-integration`
    in docker testbed.
    Screenshot hashes generated by running `compare-hashes.sh`
    in docker testbed.

---
+++ isso/js/tests/integration/compare-hashes.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/sh
+
+# This script takes the output of the screenshot puppeteer test and compares
+# the generated images against hashes of known-to-be-good images.
+#
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# Please only check in hashes rendered through the docker environment as
+# otherwise you'll be dependent on aliasing and font rendering settings on your
+# OS!
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# Requirements:
+# - imagemagick (for "identify" program)
+# - bash        (hopefully this script should run on any shell though)
+
+SCREENSHOTS_PATH=isso/js/tests/integration/screenshots
+update=false
+
+for i in "$@"; do
+  case $i in
+    -u)
+      update=true
+      shift # past argument=value
+      ;;
+    -*)
+      echo "Unknown option $i"
+      exit 1
+      ;;
+    *)
+      ;;
+  esac
+done
+
+echo "Computing screenshot hashes..."
+
+comment_hash=$(/usr/bin/identify -quiet -format "%#" $SCREENSHOTS_PATH/comment.png) || exit 1
+postbox_hash=$(/usr/bin/identify -quiet -format "%#" $SCREENSHOTS_PATH/postbox.png) || exit 1
+thread_hash=$(/usr/bin/identify -quiet -format "%#" $SCREENSHOTS_PATH/thread.png) || exit 1
+
+if [ $update = "true" ]; then
+    echo "Updating screenshot hashes..."
+    echo "$comment_hash" > $SCREENSHOTS_PATH/comment.png.hash
+    echo "$postbox_hash" > $SCREENSHOTS_PATH/postbox.png.hash
+    echo "$thread_hash" > $SCREENSHOTS_PATH/thread.png.hash
+    echo ""
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    echo "Please only check in hashes rendered through the docker environment as"
+    echo "otherwise you'll be dependent on aliasing and font rendering settings on your"
+    echo "OS!"
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    exit 0
+fi
+
+echo "Comparing hashes against reference images..."
+
+read -r expected_comment < $SCREENSHOTS_PATH/comment.png.hash
+read -r expected_postbox < $SCREENSHOTS_PATH/postbox.png.hash
+read -r expected_thread < $SCREENSHOTS_PATH/thread.png.hash
+
+failed=false
+
+if [ $comment_hash != $expected_comment ]; then
+    echo "Screenshot hash does not match for comment!"
+    echo "Expected hash:        $expected_comment"
+    echo "Computed (new) hash:  $comment_hash"
+    failed=true
+fi
+if [ $postbox_hash != $expected_postbox ]; then
+    echo "Screenshot hash does not match for postbox!"
+    echo "Expected hash:        $expected_postbox"
+    echo "Computed (new) hash:  $postbox_hash"
+    failed=true
+fi
+if [ $thread_hash != $expected_thread ]; then
+    echo "Screenshot hash does not match for thread!"
+    echo "Expected hash:        $expected_thread"
+    echo "Computed (new) hash:  $thread_hash"
+    failed=true
+fi
+
+if [ $failed = "true" ]; then
+    echo "One or more screenshots do not match (see above)."
+    echo "To update screenshot hashes, run this script with the '-u' flag"
+    exit 1
+else
+    echo "Everything fine, screenshots have not changed."
+fi
