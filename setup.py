8eca99cb842216c2105cfa304b93af62ad40213f
ix5 <ix5@users.noreply.github.com>
2022-06-05 20:17:11 +0200

    setup.py: Bump version to 0.13.0.beta0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ long_description = re_sub(r"\n## License\n.*LICENSE.*\n", "", long_description)
 
 setup(
     name='isso',
-    version='0.13.0-dev',
+    version='0.13.0.beta0',
     author='Martin Zimmermann',
     author_email='info@posativ.org',
     packages=find_packages(),
