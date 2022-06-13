bc522345d9e0a78961e714d600eab6edf80bc6a1
ix5 <ix5@users.noreply.github.com>
2022-06-13 08:13:12 +0200

    setup.py: Start on 0.13.1.dev0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ long_description = re_sub(r"\n## License\n.*LICENSE.*\n", "", long_description)
 
 setup(
     name='isso',
-    version='0.13.0',
+    version='0.13.1.dev0',
     author='Martin Zimmermann',
     author_email='info@posativ.org',
     packages=find_packages(),
