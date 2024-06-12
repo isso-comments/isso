10137631cc8f75f5e1fcd6fd42abcdd6404c6276
Chad <chadvscira@gmail.com>
2024-06-13 03:11:52 +0700

    Added i18n support for Thai

---
+++ isso/js/app/i18n/th.js
@@ -0,0 +1,38 @@
+module.exports = {
+    "postbox-text": "พิมพ์ความคิดเห็นที่นี่ (อย่างน้อย 3 ตัวอักษร)",
+    "postbox-author": "ชื่อ (ไม่บังคับ)",
+    "postbox-author-placeholder": "John Doe",
+    "postbox-email": "อีเมล (ไม่บังคับ)",
+    "postbox-email-placeholder": "johndoe@example.com",
+    "postbox-website": "เว็บไซต์ (ไม่บังคับ)",
+    "postbox-website-placeholder": "https://example.com",
+    "postbox-preview": "ตัวอย่าง",
+    "postbox-edit": "แก้ไข",
+    "postbox-submit": "ส่ง",
+    "postbox-notification": "สมัครรับการแจ้งเตือนทางอีเมลของการตอบกลับ",
+
+    "num-comments": "ความคิดเห็นหนึ่งรายการ\n{{ n }} ความคิดเห็น",
+    "no-comments": "ยังไม่มีความคิดเห็น",
+    "atom-feed": "ฟีด Atom",
+
+    "comment-reply": "ตอบกลับ",
+    "comment-edit": "แก้ไข",
+    "comment-save": "บันทึก",
+    "comment-delete": "ลบ",
+    "comment-confirm": "ยืนยัน",
+    "comment-close": "ปิด",
+    "comment-cancel": "ยกเลิก",
+    "comment-deleted": "ความคิดเห็นถูกลบแล้ว",
+    "comment-queued": "ความคิดเห็นอยู่ในคิวการตรวจสอบ",
+    "comment-anonymous": "ไม่ระบุชื่อ",
+    "comment-hidden": "{{ n }} ความคิดเห็นที่ถูกซ่อน",
+    "comment-page-author-suffix": "ผู้เขียน",
+
+    "date-now": "เมื่อสักครู่นี้",
+    "date-minute": "เมื่อหนึ่งนาทีที่แล้ว\n{{ n }} นาทีที่แล้ว",
+    "date-hour": "เมื่อหนึ่งชั่วโมงที่แล้ว\n{{ n }} ชั่วโมงที่แล้ว",
+    "date-day": "เมื่อวานนี้\n{{ n }} วันที่แล้ว",
+    "date-week": "สัปดาห์ที่แล้ว\n{{ n }} สัปดาห์ที่แล้ว",
+    "date-month": "เดือนที่แล้ว\n{{ n }} เดือนที่แล้ว",
+    "date-year": "ปีที่แล้ว\n{{ n }} ปีที่แล้ว"
+};
